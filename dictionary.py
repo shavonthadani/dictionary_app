2f693ced3c4583753facafc677841eadbdc5bfa4
shavonthadani <shavon.thadani@gmail.com>
2021-06-17 12:54:41 -0400

    comments
    Added comments to make code more readable.

--- dictionary.py
+++ dictionary.py
@@ -1,46 +1,54 @@
-from tkinter import * #might not need
+#dictionary.py
+#Shavon Thadani
+#script contains the controller and dictionary classes and creates an instance of them to create a dictionary
+#06/17/2021
+from tkinter import *
 from model import Model
 import re
 import threading
 from PIL import Image, ImageTk
 from gifObject import ImageLabel
 import readonlyText
-#from controller import Controller
 class View(Tk):
 
     def __init__(self, controller):
+        #constructor
         self.controller = controller
         super().__init__()
         self.bind('<Return>',self.controller.on_return_key)
         self.wm_title("Dictionary")
         self.input = StringVar()
-
+    #create page fliping gif for loading screen
     def _create_gif(self):
         self.gif = ImageLabel(self)
         self.gif.load('page_flip.gif')
         self.gif.grid(row=0,column=0)
+    #create the pic for the definition
     def _create_def_pic(self):
-        #page.png is the second image
-        #start.png is the first image
         def_pic = ImageTk.PhotoImage(Image.open("page.png"))
         self.lbl_def_image = Label(self, image=def_pic)
         self.lbl_def_image.image = def_pic
         self.lbl_def_image.grid(row=0,column=0)
+    #create the starting pic
     def _create_start_pic(self):
         start_pic = ImageTk.PhotoImage(Image.open("start_pic.png"))
         self.lbl_start_image = Label(self, image=start_pic)
         self.lbl_start_image.image = start_pic
         self.lbl_start_image.grid(row=0,column=0)
+    #create the label to prompt the user to enter a word
     def _create_prompt_label(self):
         self.lbl_prompt = Label(self, text = "Enter a word:",bg = "#90D395")
         self.lbl_prompt.place(x=250,y=53)
+    #create the entry box for the user to input a word
     def _create_entry(self):
         self.ent_input = Entry(self, textvariable=self.input)
         self.ent_input.place(x=350,y=50)
+    #create the label to display the word in the dictionary
     def _create_word_label(self):
         self.lbl_word = Label(self, font="helvetica 20", wraplength=175, justify="left")
         self.lbl_word.place(x=220,y=200)
         self.lbl_word.lower()
+    #create the textbox with scrollbar for the definition
     def _create_txt_with_scroll(self):
         self.txt_def = readonlyText.ROText(self, font="helvetica 10", height=20, width=30, wrap= WORD, borderwidth=0)
         self.txt_def.bind("<Key>", lambda e: "break")
@@ -62,13 +70,14 @@ class View(Tk):
         self.mainloop()
 
 class Controller:
-
+    #constructor
     def __init__(self):
         self.model = Model()
         self.view = View(self)
 
     def main(self):
         self.view.main()
+    #thread to retrieve the definition from database and change the position of widgets to display the definition
     def loadDef(self):
         dic_word = self.model.translate(self.view.input.get())
         self.view.lbl_word['text'] = dic_word[0]
@@ -84,6 +93,7 @@ class Controller:
         self.view.lbl_prompt.lift()
         self.view.ent_input.delete(0,END)
         self.view.ent_input.lift()
+    #method gets executed by the return key event. If the word contains of only alphabet or spaces, it will start a thread. It will also display the gif as a loading screen.
     def on_return_key(self, event):
         if re.search(r"^[a-zA-Z ]+$",self.view.input.get()) != None:
             load_def_thread = threading.Thread(target=self.loadDef)
--- model.py
+++ model.py
@@ -1,10 +1,14 @@
+#model.py
+#Shavon Thadani
+#Script is the backend for dictionary.py
+#06/17/2021
 import mysql.connector
 from difflib import get_close_matches
 
 class Model:
-
+    #returns a tuple of the word and the definition
     def translate(self, w):
-        #create connection
+        #create connection to database
         global con
         global cursor
         con = mysql.connector.connect(
@@ -21,7 +25,7 @@ class Model:
 
         w = w.lower()
 
-        #print results
+        #format the results and return
         if results:
             number = 0
             definition = ""
@@ -30,22 +34,25 @@ class Model:
                 definition += str(number) + ". " + result[0] + "\n"
             return (w, definition)
 
-        #Gets words to look for similarities might have error
+        #If not exact match
+        #get all words from database, compare how similar they are, find definition for most similar word
         word_search = cursor.execute("SELECT Expression FROM Dictionary")
         words = cursor.fetchall()
         wordsf = []
         for word in words:
             wordsf.append(word[0])
+        #found a similar result
         if len(get_close_matches(w,wordsf)) > 0:
             word = get_close_matches(w, wordsf)[0]
-
             sec_search = cursor.execute("SELECT Definition FROM Dictionary WHERE Expression = '%s'" % word)
             sec_def = cursor.fetchall()
             definition = ""
             num =0
+            #format output and return
             for result in sec_def:
                 num = num + 1
                 definition += str(num) + ". "+ result[0] + "\n"
             return (word, definition)
+        #no similar word found
         else:
             return(w,"No word found!")
